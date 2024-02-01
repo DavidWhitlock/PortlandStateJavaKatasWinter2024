a3edeb874570399c889310c182b8c76012230703
Xerasin <phoenixteam20@gmail.com>
2024-01-31 20:24:09 -0800

    Katie Driving

--- leapyears/pair3/src/it/java/edu/pdx/cs410J/pair3/LeapYearsIT.java
+++ leapyears/pair3/src/it/java/edu/pdx/cs410J/pair3/LeapYearsIT.java
@@ -21,6 +21,38 @@ class LeapYearsIT extends InvokeMainTestCase {
   }
 
   @Test
+  void test2019NotLeapYear() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "2019");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Is not a Leap Year!"));
+  }
+
+  @Test
+  void test1201NotLeapYear() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "1201");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Is not a Leap Year!"));
+  }
+
+  @Test
+  void test1200LeapYear() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "1200");
+    assertThat(result.getTextWrittenToStandardOut(), containsString("Is a Leap Year!"));
+  }
+  @Test
+  void testTwoArguments() {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "2020", "word");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Too many arguments!"));
+  }
+
+  @Test
+  void testNegativeYear()
+  {
+    InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "-2020");
+    assertThat(result.getTextWrittenToStandardError(), containsString("Year cannot be negative!"));
+  }
+
+
+
+  @Test
   void notANumberArg(){
     InvokeMainTestCase.MainMethodResult result = invokeMain(LeapYears.class, "word");
     assertThat(result.getTextWrittenToStandardError(), containsString("Not a number"));
