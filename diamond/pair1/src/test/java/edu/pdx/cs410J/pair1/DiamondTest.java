de789d4b7f4d49a59e23e1a897858640c9313e18
jchier <chier@pdx.edu>
2024-02-07 19:50:43 -0800

    John made a it test

--- diamond/pair1/src/test/java/edu/pdx/cs410J/pair1/DiamondTest.java
+++ diamond/pair1/src/test/java/edu/pdx/cs410J/pair1/DiamondTest.java
@@ -1,14 +1,18 @@
 package edu.pdx.cs410J.pair1;
 
-import edu.pdx.cs410J.InvokeMainTestCase;
 import org.junit.jupiter.api.Test;
-
 public class DiamondTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Diamond();
+  }
+
+  @Test
+  void invalidArgument(){
+
+
   }
 
 }
