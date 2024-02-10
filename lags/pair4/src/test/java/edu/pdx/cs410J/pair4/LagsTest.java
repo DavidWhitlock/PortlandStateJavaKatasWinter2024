43770935edb4fb6ff27e8414e7f961619fdd086d
David Whitlock <whitlock@cs.pdx.edu>
2024-02-10 15:17:34 -0800

    Lags Katas (#1)

---
+++ lags/pair4/src/test/java/edu/pdx/cs410J/pair4/LagsTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.pair4;
+
+import org.junit.jupiter.api.Test;
+
+public class LagsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Lags();
+  }
+
+}
---
+++ lags/pair5/src/test/java/edu/pdx/cs410J/pair5/LagsTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.pair5;
+
+import org.junit.jupiter.api.Test;
+
+public class LagsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Lags();
+  }
+
+}
