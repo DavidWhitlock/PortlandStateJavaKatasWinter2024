95b45249ed80c5ffc9b915ddb400ecd94fdec46a
Sharon Oba <soba@pdx.edu>
2024-02-10 10:19:35 -0800

    standard out for diamond

--- diamond/pair2/src/it/java/edu/pdx/cs410J/pair2/DiamondIT.java
+++ diamond/pair2/src/it/java/edu/pdx/cs410J/pair2/DiamondIT.java
@@ -1,7 +1,6 @@
 package edu.pdx.cs410J.pair2;
 
 import edu.pdx.cs410J.InvokeMainTestCase;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,18 +8,16 @@ import static org.hamcrest.core.StringContains.containsString;
 
 class DiamondIT extends InvokeMainTestCase {
 
-  @Disabled
   @Test
   void invokingMainWithNoArgumentsPrintsMissingArgumentsToStandardError() {
     InvokeMainTestCase.MainMethodResult result = invokeMain(Diamond.class);
     assertThat(result.getTextWrittenToStandardError(), containsString("Missing command line arguments"));
   }
 
-  @Disabled
   @Test
   void gDiamondValid() {
     InvokeMainTestCase.MainMethodResult result = invokeMain(Diamond.class,"g");
-    assertThat(result.getTextWrittenToStandardError(), containsString("      A\n" +
+    assertThat(assertThat(result.getTextWrittenToStandardError(), containsString("      A\n" +
             "     B B\n" +
             "    C   C\n" +
             "   D     D\n" +
