8a48fb18d261cc045bc1d170cd87ad147274064d
Xerasin <phoenixteam20@gmail.com>
2024-02-14 20:46:30 -0800

    Switching

--- lags/pair2/src/main/java/edu/pdx/cs410J/pair2/Lags.java
+++ lags/pair2/src/main/java/edu/pdx/cs410J/pair2/Lags.java
@@ -2,6 +2,8 @@ package edu.pdx.cs410J.pair2;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.Collection;
+
 /**
  * A class for getting started with a code kata
  *
@@ -13,5 +15,13 @@ public class Lags {
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
+  }
+
+  public int getProfit(Collection<Flight> flights) {
+
+    return 0;
+  }
+  public boolean checkOverlap(Collection<Flight> flights) {
+    return false;
   }
 }
