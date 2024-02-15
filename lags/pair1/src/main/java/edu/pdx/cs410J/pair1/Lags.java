69f11ca49363fa16bfc0a6e6a6f01314c53da083
nowen <nowen@cecs.pdx.edu>
2024-02-14 21:00:48 -0800

    added all  functions

--- lags/pair1/src/main/java/edu/pdx/cs410J/pair1/Lags.java
+++ lags/pair1/src/main/java/edu/pdx/cs410J/pair1/Lags.java
@@ -12,6 +12,39 @@ public class Lags {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    int num_flights = Integer.parseInt(args[0]);
+    String[] flights = new String[num_flights];
+    for(int i = 0; i < num_flights; i++)
+    {
+      flights[i] = args[i+1];
+    }
+    Trip[] trips = new  Trip[num_flights];
+    String ID = null;
+
+    for(int i = 0; i < num_flights; i++) {
+      String[] flight_details = flights[i].split(" ");
+      Trip  trip = new Trip(flight_details[0], Integer.parseInt(flight_details[1]), Integer.parseInt(flight_details[2]), Integer.parseInt(flight_details[3]));
+      trips[i] = trip;
+      trips[i].print_trip();
+    }
+    int max_money = 0;
+    int money = 0;
+    for(int i = 0; i < num_flights; i++) {
+      for(int j = i + 1; j < num_flights; j++) {
+        money = trips[i].Comparetrip(trips[j]);
+        if (money >= max_money) {
+          max_money = money;
+        }
+      }
+    }
+    for(int i = 0; i < num_flights; i++) {
+      if (trips[i].get_cost() > max_money) {
+        max_money = trips[i].get_cost();
+      }
+    }
+    System.out.println("Max Profit: $" + max_money);
   }
+
+
+
 }
