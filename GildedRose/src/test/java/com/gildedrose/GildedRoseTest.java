83a788e1f8a7cf6a1a25fc5ca7cb8af0fc74483e
David Whitlock <david.m.whitlock@gmail.com>
2024-02-21 19:02:49 -0800

    Added unit tests for the GildenRose class so that we can confidently refactor it.

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,17 +3,109 @@ package com.gildedrose;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    @Disabled
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        String name = "foo";
+        Item[] items = new Item[] { new Item(name, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(name, equalTo(app.items[0].name));
     }
 
+    @Test
+    void regularItemDecreasesInQualityByOneAtTheEndOfTheDay() {
+        Item[] items = new Item[] { new Item("Test Item", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(9));
+        assertThat(item.quality, equalTo(19));
+    }
+
+    @Test
+    void agedBrieIncreasesInQualityWhenLessThanTwoDaysBeforeSellInDate() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(1));
+        assertThat(item.quality, equalTo(1));
+    }
+
+    @Test
+    void agedBrieHasAMaximumQualityOf50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(1));
+        assertThat(item.quality, equalTo(50));
+    }
+
+    @Test
+    @Disabled("Conjured Items don't work yet")
+    void conjuredItemHasQualityThatDecreasesByTwo() {
+        Item[] items = new Item[] { new Item("Conjured Item", 3, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(2));
+        assertThat(item.quality, equalTo(4));
+    }
+
+    @Test
+    void backstagePassHasQualityOfZeroTheDayAfterSellIn() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 57) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(-1));
+        assertThat(item.quality, equalTo(0));
+    }
+
+    @Test
+    void regularItemHasQualityDecreaseByTwoTheDayAfterSellIn() {
+        Item[] items = new Item[] { new Item("Regular Item", 0, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(-1));
+        assertThat(item.quality, equalTo(18));
+    }
+
+    @Test
+    void backstagePassIncreasesQualityByTwo10DaysBeforeSellIn() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        for (int i = 1; i <= 5; i++) {
+            app.updateQuality();
+        }
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(5));
+        assertThat(item.quality, equalTo(30));
+    }
+
+    @Test
+    void sulfurasNeverDecreasesInQuality() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(0));
+        assertThat(item.quality, equalTo(80));
+    }
 }
