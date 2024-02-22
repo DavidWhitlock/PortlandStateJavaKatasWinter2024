45781d696eee820ae0e320684abe605f4d5e2a22
Sharon Oba <soba@pdx.edu>
2024-02-21 19:41:35 -0800

    well known names are constants

--- GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -3,17 +3,51 @@ package com.gildedrose;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    @Disabled
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        String name = "foo";
+        Item[] items = new Item[] { new Item(name, 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertThat(name, equalTo(app.items[0].name));
+    }
+
+    @Test
+    void decreaseQualityByOneDayRegularItem() {
+        Item[] items = new Item[] { new Item("Regular Item", 10, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(9));
+        assertThat(item.quality, equalTo(19));
+    }
+    @Test
+    void agedBrieIncreasesInQualityWhenThan2DaysLeft(){
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(1));
+        assertThat(item.quality, equalTo(1));
+    }
+
+    @Test
+    void agedBrieHasMaxQualityofFifty(){
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        Item item = app.items[0];
+        assertThat(item.sellIn, equalTo(1));
+        assertThat(item.quality, equalTo(50));
     }
 
 }
