106cda30095b697fb9d2c9cc1396ca7f0ad59746
David Whitlock <david.m.whitlock@gmail.com>
2024-02-25 13:52:46 -0800

    Add code for the RPN Calculator kata.

---
+++ rpn/mob1/src/test/java/edu/pdx/cs410J/mob1/RPNCalculatorTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.mob1;
+
+import org.junit.jupiter.api.Test;
+
+public class RPNCalculatorTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new RPNCalculator();
+  }
+
+}
