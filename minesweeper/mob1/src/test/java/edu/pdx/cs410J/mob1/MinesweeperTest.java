8bae195530f5a797ef00ef63ab2a2f7de38c7b99
David Whitlock <david.m.whitlock@gmail.com>
2024-02-25 13:57:07 -0800

    Add code for the Minesweeper kata.

---
+++ minesweeper/mob1/src/test/java/edu/pdx/cs410J/mob1/MinesweeperTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.mob1;
+
+import org.junit.jupiter.api.Test;
+
+public class MinesweeperTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Minesweeper();
+  }
+
+}
---
+++ minesweeper/mob2/src/test/java/edu/pdx/cs410J/mob2/MinesweeperTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.mob2;
+
+import org.junit.jupiter.api.Test;
+
+public class MinesweeperTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new Minesweeper();
+  }
+
+}
