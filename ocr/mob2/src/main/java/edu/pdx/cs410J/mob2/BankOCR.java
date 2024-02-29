50a462c36e05bb9d4e8facbc9815ee99a330df9e
Sharon Oba <soba@pdx.edu>
2024-02-28 19:47:49 -0800

    character zero and one UnitTest passes

--- ocr/mob2/src/main/java/edu/pdx/cs410J/mob2/BankOCR.java
+++ ocr/mob2/src/main/java/edu/pdx/cs410J/mob2/BankOCR.java
@@ -14,4 +14,15 @@ public class BankOCR {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  public int firstString(String zero) {
+    String[] zeroArray = zero.split("\n");
+    if (zeroArray[0].equals(" _ ") && zeroArray[1].equals("| |") && zeroArray[2].equals("|_|")) {
+      return 0;
+    }
+    else if(zeroArray[0].equals("   ") && zeroArray[1].equals("  |") && zeroArray[2].equals("  |")){
+      return 1;
+    }
+    return -1;
+  }
 }
--- ocr/mob2/src/test/java/edu/pdx/cs410J/mob2/BankOCRTest.java
+++ ocr/mob2/src/test/java/edu/pdx/cs410J/mob2/BankOCRTest.java
@@ -2,6 +2,9 @@ package edu.pdx.cs410J.mob2;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class BankOCRTest
 {
 
@@ -10,4 +13,21 @@ public class BankOCRTest
     new BankOCR();
   }
 
+  @Test
+  void characterIsZeroReturnsZero() {
+    BankOCR bankOCR = new BankOCR();
+    String zero = " _ \n" +
+            "| |\n" +
+            "|_|\n";
+    assertThat(bankOCR.firstString(zero), equalTo(0));
+  }
+
+  @Test
+  void characterIsOneReturnsOne() {
+    BankOCR bankOCR = new BankOCR();
+    String one = "   \n" +
+            "  |\n" +
+            "  |\n";
+    assertThat(bankOCR.firstString(one), equalTo(1));
+  }
 }
