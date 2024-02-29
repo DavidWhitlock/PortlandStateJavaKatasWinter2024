dc6d4e1964c4a14d50124befd81a89df7a2dab03
benjaminlutz <benlutz@pdx.edu>
2024-02-28 18:21:14 -0800

    updated the kata

---
+++ Tennis/src/main/java/edu/pdx/cs410J/mob/Player.java
@@ -0,0 +1,8 @@
+package edu.pdx.cs410J.mob;
+
+public class Player {
+    int score = 0;
+    public int getScore() {
+       return this.score;
+    }
+}
--- Tennis/src/test/java/edu/pdx/cs410J/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs410J/mob/TennisTest.java
@@ -2,12 +2,21 @@ package edu.pdx.cs410J.mob;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class TennisTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Tennis();
+  }
+
+  @Test
+  void playerHasInitialScoreOfLove() {
+    Player player = new Player();
+    assertThat(player.getScore(), equalTo(0));
   }
 
 }
