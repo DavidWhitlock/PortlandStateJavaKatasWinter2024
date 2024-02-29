ac839e7adfda10ab93c51b2172b66536ac167507
Nora Luna <nob@pdx.edu>
2024-02-28 20:29:42 -0800

    Nora driving

--- ocr/mob1/src/main/java/edu/pdx/cs410J/mob1/Digit.java
+++ ocr/mob1/src/main/java/edu/pdx/cs410J/mob1/Digit.java
@@ -1,13 +1,22 @@
 package edu.pdx.cs410J.mob1;
 
+import java.util.Arrays;
+
 public class Digit {
 
-    enum Number{ONE};
+    static String[] one = {"   ", "  |", "  |"};
     private int number;
     public Digit(String[] arrayNum){
-
+        number = parseDigit(arrayNum);
     }
     public int getNum() {
         return number;
+    }
+
+    public int parseDigit(String[] array) {
+        if (Arrays.equals(array, one)) {
+            return 1;
+        }
+        return 0;
     }
 }
