9218be57f5659aacf0e22ebf130e93c0ed34fe72
amanda-gutierrez <agutier2@pdx.edu>
2024-03-06 19:43:01 -0800

    first driver

--- rpn/mob1/src/test/java/edu/pdx/cs410J/mob1/RPNCalculatorTest.java
+++ rpn/mob1/src/test/java/edu/pdx/cs410J/mob1/RPNCalculatorTest.java
@@ -1,13 +1,26 @@
 package edu.pdx.cs410J.mob1;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RPNCalculatorTest
 {
 
+  @Disabled
   @Test
   void canInstantiateKataClass() {
-    new RPNCalculator();
+    //new RPNCalculator();
+  }
+
+  @Test
+  void eightAndNineAddTo17(){
+    String test = "8 9 +";
+    RPNCalculator rpn = new RPNCalculator(test);
+
+    assertThat(rpn.result(), equalTo(17));
   }
 
 }
